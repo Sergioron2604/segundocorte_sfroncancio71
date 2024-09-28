12b4c2ac5cb6d149b3c802e9c0d6f28c3a3371da
Sergioron2604 <roncanciofernando@gmail.com>
2024-09-28 00:17:47 -0500

    Se realiza la media query sobre menu de hamburguesa

--- Mockup/Fruteria.js
+++ Mockup/Fruteria.js
@@ -1,19 +1,12 @@
-// script.js
 
 function filterProducts(category) {
-    // Obtener todos los elementos de las tarjetas de productos
     const products = document.querySelectorAll('.product-card');
-
-    // Recorrer cada tarjeta de producto
     products.forEach(product => {
-        // Obtener la categoría de la tarjeta de producto
         const productCategory = product.getAttribute('data-category');
-
-        // Mostrar u ocultar la tarjeta de producto según la categoría seleccionada
         if (category === 'all' || productCategory === category) {
-            product.style.display = 'block'; // Mostrar producto
+            product.style.display = 'block';
         } else {
-            product.style.display = 'none'; // Ocultar producto
+            product.style.display = 'none';
         }
     });
 }
@@ -23,14 +16,16 @@ const mobileMenu = document.getElementById('mobile-menu');
 const navList = document.querySelector('.nav-list');
 
 // Mostrar y ocultar el menú al hacer clic en el botón del menú hamburguesa
-mobileMenu.addEventListener('click', () => {
-    navList.classList.toggle('active'); // Alterna la clase activa para mostrar/ocultar el menú
-});
+if (mobileMenu) {
+    mobileMenu.addEventListener('click', () => {
+        navList.classList.toggle('active');
+    });
+}
 
 // Cierra el menú si se hace clic en cualquier parte del documento
 document.addEventListener('click', function(event) {
     if (!event.target.closest('nav')) {
-        navList.classList.remove('active'); // Cierra el menú si se hace clic fuera de él
+        navList.classList.remove('active');
     }
 });
 
@@ -40,16 +35,14 @@ const dropdownMenu = document.querySelector('.dropdown-menu');
 
 if (dropdownToggle) {
     dropdownToggle.addEventListener('click', (event) => {
-        event.preventDefault(); // Evitar el comportamiento por defecto del enlace
-        dropdownMenu.classList.toggle('active'); // Alterna la visibilidad del menú desplegable
+        event.preventDefault();
+        dropdownMenu.classList.toggle('active');
     });
 }
 
 // Cierra el menú desplegable si se hace clic fuera de él
 document.addEventListener('click', function(event) {
     if (!event.target.closest('.dropdown-toggle') && !event.target.closest('.dropdown-menu')) {
-        dropdownMenu.classList.remove('active'); // Cierra el menú desplegable si se hace clic fuera de él
+        dropdownMenu.classList.remove('active');
     }
 });
-
-
